87086b0c2c33c43fea1114759a86bbf15922fc69
Ayu189 <tesfayeayele496@gmail.com>
2022-12-08 05:39:54 -0800

    My print commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point 
- * Return: Always 0 (success)
+ * main - main block
+ * Return: 0
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }

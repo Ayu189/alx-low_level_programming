1e28d60a66f10997c499d9ae2c089f1392142fd6
Ayu189 <tesfayeayele496@gmail.com>
2022-12-08 04:27:04 -0800

    print commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,9 @@
  * main - Entry point 
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	return (0);
 }
